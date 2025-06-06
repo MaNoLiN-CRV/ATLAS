51f41a3fc5cfdfbf4ced7669e2a9aa76c36e042a
MaNoLiN-CRV <manoliin.27@gmail.com>
2025-06-06 11:47:21 +0200

    fix: Update make_backup method to support compression option and enhance backup creation logic

--- src/utils/db_utils/backup_maker.py
+++ src/utils/db_utils/backup_maker.py
@@ -12,25 +12,41 @@ class BackupMaker:
     TO DISK = '{target_backup_file_path}'
     WITH FORMAT, INIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10;
     """
+    
+    SQL_BACKUP_COMPRESSED_TEMPLATE: str = """
+    BACKUP DATABASE [{db_name}]
+    TO DISK = '{target_backup_file_path}'
+    WITH FORMAT, INIT, SKIP, NOREWIND, NOUNLOAD, STATS = 10, COMPRESSION;
+    """
 
     def __init__(self, connector: MSSQLConnector):
         self.connector = connector
         self.database_name = ConfigManager.database
 
-    def make_backup(self, backup_file_path: str) -> bool: 
+    def make_backup(self, backup_file_path: str , compress: bool = True) -> bool: 
         """
         Creates a database backup at the specified file path.
         """
         correct = True
         try:
-            # Format the SQL query using the instance's database_name
-            # and the provided backup_file_path.
-            final_query = self.SQL_BACKUP_TEMPLATE.format(
-                db_name=self.database_name,
-                target_backup_file_path=backup_file_path
-            )
+            # Seleccionar el template adecuado según si se requiere compresión
+            if compress:
+                final_query = self.SQL_BACKUP_COMPRESSED_TEMPLATE.format(
+                    db_name=self.database_name,
+                    target_backup_file_path=backup_file_path
+                )
+                print(f"Creating compressed backup of database '{self.database_name}'...")
+            else:
+                final_query = self.SQL_BACKUP_TEMPLATE.format(
+                    db_name=self.database_name,
+                    target_backup_file_path=backup_file_path
+                )
+                print(f"Creating uncompressed backup of database '{self.database_name}'...")
+            
             self.connector.execute_query(final_query)
-            print(f"Backup of database '{self.database_name}' created at '{backup_file_path}'")
+            
+            compression_status = "compressed" if compress else "uncompressed"
+            print(f"{compression_status.capitalize()} backup of database '{self.database_name}' created at '{backup_file_path}'")
         except Exception as e:
             print(f"Error creating backup: {e}")
             correct = False
